889b793079f8ebfb8a0bb6cb49e12f75792ac0a1
Katherine Ude <Katie.Ude@gmail.com>
2015-11-19 15:05:28 -0600

    saving updates to sinatra boilerplate

--- app.rb
+++ app.rb
@@ -2,10 +2,14 @@ require 'bundler'
 Bundler.require
 
 get '/' do
-  today = {
-    :November => 19
-  }
+  # today = {:November => 19}.to_json
 
-  return today.to_json
+  erb :hello
+
+end
+
+get '/worlds' do
+
+  erb :world
 
 end
